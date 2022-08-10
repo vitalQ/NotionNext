c8439f8566a9a8f0ea7da76931b5347670f17844
vitalq <37363257+vitalQ@users.noreply.github.com>
2022-08-10 14:10:05 +0800

    Update blog.config.js
    Vital-3efcf94cc29d47f5ba173e00fdeb69bd

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   BIO: '', // 作者简介
   LINK: 'https://vitalblog.com', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
-  NOTION_PAGE_ID:
+  NOTION_PAGE_ID: 'Vital-3efcf94cc29d47f5ba173e00fdeb69bd',
   process.env.NOTION_PAGE_ID || 'Vital-3efcf94cc29d47f5ba173e00fdeb69bd', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
   DEBUG: process.env.NEXT_PUBLIC_DEBUG || false, // 是否显示调试按钮
